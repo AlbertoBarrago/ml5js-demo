6e8f981a437153772b73bb9cad7697571e624b14
Alberto Barrago <albertobarrago@gmail.com>
2025-12-16 22:47:33 +0100

    🐛 fix: Center canvas for bodySegmentation

--- src/poc/js/bodySegmentation.js
+++ src/poc/js/bodySegmentation.js
@@ -3,31 +3,31 @@ let segmentation;
 let video;
 
 let options = {
-    maskType: "parts",
+	maskType: "parts",
 };
 
 function preload() {
-    bodySegmentation = ml5.bodySegmentation("BodyPix", options);
+	bodySegmentation = ml5.bodySegmentation("BodyPix", options);
 }
 
 function setup() {
-    var cnv = createCanvas(880, 640);
-    cnv.center('horizontal');
-    video = createCapture(VIDEO);
-    video.size(640, 480);
-    video.hide();
-    // Start body segmentation
-    bodySegmentation.detectStart(video, gotResults);
+	var cnv = createCanvas(640, 480);
+	cnv.center('horizontal');
+	video = createCapture(VIDEO);
+	video.size(640, 480);
+	video.hide();
+	// Start body segmentation
+	bodySegmentation.detectStart(video, gotResults);
 }
 
 function gotResults(result) {
-    segmentation = result;
+	segmentation = result;
 }
 
 function draw() {
-    background(255);
-    image(video, 0, 0);
-    if (segmentation) {
-        image(segmentation.mask, 0, 0, width, height);
-    }
+	background(255);
+	image(video, 0, 0);
+	if (segmentation) {
+		image(segmentation.mask, 0, 0, width, height);
+	}
 }
